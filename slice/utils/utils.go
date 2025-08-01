afadf4e9e78134b38402d448481314aff83d47ed
lsham <yuann1035@gmail.com>
2025-08-01 18:29:52 +0900

    Feat: slice kmeans update

--- slice/utils/utils.go
+++ slice/utils/utils.go
@@ -2,6 +2,7 @@ package utils
 
 import (
 	"fmt"
+	"math"
 )
 
 /*
@@ -26,4 +27,63 @@ func Dot(a, b []float64) (float64, error) {
 	}
 
 	return sum, nil
+}
+
+func Transpose(source [][]float64) [][]float64 {
+	c := len(source[0])
+
+	res := make([][]float64, len(source))
+	for i := range res {
+		res[i] = make([]float64, c)
+	}
+
+	for i, row := range source {
+		for j, col := range row {
+			res[j][i] = col
+		}
+	}
+
+	return res
+}
+
+func EuclideanDistance(p1, p2 []float64) (float64, error) {
+	if len(p1) != len(p2) {
+		return -1, fmt.Errorf("mismatched dimensions")
+	}
+
+	total := 0.0
+
+	for i, x := range p1 {
+		total += math.Pow(x-p2[i], 2)
+	}
+
+	return math.Sqrt(total), nil
+}
+
+func MaxIndex(arr []float64) int {
+	idx := 0
+	max := math.Inf(-1)
+
+	for i, v := range arr {
+		if v > max {
+			max = v
+			idx = i
+		}
+	}
+
+	return idx
+}
+
+func MinIndex(arr []float64) int {
+	idx := 0
+	min := math.MaxFloat64
+
+	for i, v := range arr {
+		if v < min {
+			min = v
+			idx = i
+		}
+	}
+
+	return idx
 }
